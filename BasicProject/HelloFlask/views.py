2b648edaa8961a88dcb4c94abfc50f2c6dfd7929
huangwuyi <792786297@qq.com>
2018-06-27 09:33:23 +0800

    3-2

--- BasicProject/HelloFlask/views.py
+++ BasicProject/HelloFlask/views.py
@@ -13,5 +13,8 @@ def home():
         title="Hello Flask",
         message="Hello Flask!",
         content = " on " + formatted_now)
+@app.route('/api/data')
+def get_data():
+    return app.send_static_file('data.json')
 
 
